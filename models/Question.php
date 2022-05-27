22185ecb9f23f5e9e224a9b95c1e578114e5d2f1
Alexander Wenzel <alexander.wenzel.berlin@gmail.com>
2022-05-27 05:01:56 +0200

    implemented translation support

--- models/Question.php
+++ models/Question.php
@@ -8,9 +8,26 @@ use October\Rain\Database\Traits\Sortable;
 class Question extends Model
 {
     use Sortable;
+
     public $table = 'bc_questions';
     public $belongsTo = [
-        'faq' => 'FAQ',
+        'faq'   => 'FAQ',
         'order' => 'sort_order',
+    ];
+
+    /**
+     * @var array
+     */
+    public $implement = [
+        'RainLab.Translate.Behaviors.TranslatableModel',
+    ];
+
+    /**
+     * @var array
+     */
+    public array $translatable = [
+        "question",
+        "answer",
+        "details",
     ];
 }
